07d0e37230341d22a04112fc620cb1f820ae9a00
PujaPatel <ppate343@uwo.ca>
2021-11-14 16:10:57 -0500

    created express server

---
+++ index.js
@@ -0,0 +1,6 @@
+//importing express library downloaded from npm install express
+const { application } = require('express');
+const express = require('express'); 
+
+//execution returns an instance of the server, (app is just a convention can be named anything)
+const app = express(); 
