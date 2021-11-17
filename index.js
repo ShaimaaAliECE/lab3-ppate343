d33e2efc7e2e83306ce347df8cf6db83de2ef0dc
PujaPatel <ppate343@uwo.ca>
2021-11-17 12:16:18 -0500

    added build

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 //importing express library downloaded from npm install express
-const { application } = require('express');
+const { application, response } = require('express');
 const express = require('express'); 
+
+//let logging = require('./login-form.html'); 
 
 //execution returns an instance of the server, (app is just a convention can be named anything)
 const app = express(); 
 
-app.get()
+app.use(express.static('static')); 
+
+app.get('/', (req,res) => {
+    res.send('<h1> Welcome To The Doodle App! </h1>'); 
+}); 
+
+//app.use(express.urlencoded({
+  //  extended: true 
+//}))
+
+
+app.post('/login', (req,res) => {
+//logging in
+let userName = req.body.usr; 
+let password = req.body.pwd; 
+let message = "Access Denied"; 
+if (userName == "admin" && password == '123') {
+    message = 'Welcome'; 
+}
+res.send(message); 
+})
+app.use(express.static('static'))
+
+app.listen(1500); 
